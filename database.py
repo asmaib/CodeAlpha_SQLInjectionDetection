e85167a9adfdb27f88eec8a0f7250b2b0a8acaea
asmaib <asma566.9a@gmail.com>
2025-06-28 00:10:32 +0300

    Switched to SQLite for simplicity

--- database.py
+++ database.py
@@ -2,17 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-MYSQL_USERNAME = "root"
-MYSQL_PASSWORD = "root" 
-MYSQL_HOST = "localhost"
-MYSQL_PORT = "3306"
-MYSQL_DB = "codealpha_sql_proj"
 
-
-DATABASE_URL = f"mysql+pymysql://{MYSQL_USERNAME}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}"
+DATABASE_URL = "sqlite:///./sqlidb.db"
 
 engine = create_engine(DATABASE_URL)
-
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
